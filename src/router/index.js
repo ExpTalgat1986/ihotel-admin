8e4329fd257a3fbac9388770748069e73cf7dc1a
Shadiyar <abdiyev096@mail.ru>
2022-05-06 22:29:17 +0600

    done

--- src/router/index.js
+++ src/router/index.js
@@ -28,13 +28,7 @@ const allowedRoutesForWaiters = ['main.messages', 'main.orders', 'main.layout']
  * with the Router instance.
  */
 
-export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : process.env.VUE_ROUTER_MODE === 'history'
-    ? createWebHistory
-    : createWebHashHistory
-
+export default route(function(/* { store, ssrContext } */) {
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
@@ -42,7 +36,6 @@ export default route(function (/* { store, ssrContext } */) {
     // Leave this as is and make changes in quasar.conf.js instead!
     // quasar.conf.js -> build -> vueRouterMode
     // quasar.conf.js -> build -> publicPath
-    history: createHistory(process.env.MODE === 'ssr' ? void 0 : process.env.VUE_ROUTER_BASE),
   })
 
   Router.beforeEach((to, from, next) => {
